1479f3ed329e5e8a577f94bdc75b4dd3780f18d3
GoorgeBNXN <148658691+GoorgeBNXN@users.noreply.github.com>
2024-01-30 16:22:43 +0100

    Delete receipt.js

--- receipt.js
+++
@@ -1,7 +0,0 @@
-
-const view_receipt = document.getElementById('btnIntent');
-
-view_receipt.addEventListener('click', function () {
-    let receiptIntent = document.getElementById('right-ctrl');
-        receiptIntent.checked = false;
-});
